e375886aa1fd0f900c61900d283ce0648b4581fe
cvsgbhn <kbh123456g@gmail.com>
2021-10-04 23:14:58 +0300

    some improvements i guess... i'm too slow...

--- pkg/app/logic/shorten.go
+++ pkg/app/logic/shorten.go
@@ -8,15 +8,28 @@ import (
 	"fmt"
 )
 
-func byteToString62(initialText []byte) string {}
+func base62Convert(initNum []byte) string {
+	alphabet := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}
 
 func ShortenLink(longLink string) models.LinkInfo {
 	data := []byte(longLink)
 	hashedLink := md5.Sum(data)
 
 	var newLink models.LinkInfo
-	//newLink.Hash = string(hashedLink[:])[:5]
-	newLink.Hash = (hex.EncodeToString(hashedLink[:]))[0:5]
+	newLink.OriginalUrl = longLink
+	for i := 0; i <= len(hashedLink[:])- 4; i++ {
+		fmt.Println(hex.EncodeToString(hashedLink[:]))
+		fmt.Println(hashedLink[:])
+		fmt.Println(string(hashedLink[:]))
+		tempHash := (hex.EncodeToString(hashedLink[:]))[i:i+4]
+		checkHash := models.GetByShortenedLink(tempHash)
+		if len(checkHash) == 0 {
+			newLink.Hash = tempHash
+			models.AddLink(&newLink)
+			return newLink	
+		}
+	}
 
 	fmt.Println(hashedLink[:])
 	fmt.Println(newLink.Hash)
